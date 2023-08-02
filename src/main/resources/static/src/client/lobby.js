2ba597ef6e30fa9808d276ddff291701d443a25b
keyleeu <minhk.lee21@gmail.com>
2023-08-02 18:34:55 +0900

    4616746734671346

--- src/main/resources/static/src/client/lobby.js
+++ src/main/resources/static/src/client/lobby.js
@@ -10,67 +10,39 @@ let flag = true;
 
 Promise.all([
 
-  downloadAssets(),
   pixiApp(),
   getRobotInfos(),
 
 ]).then(() => {
 
-  const username = sessionStorage.getItem('username');
-  console.log(username);
+  // const username = sessionStorage.getItem('username');
+  // console.log(username);
+
+  startButton.onclick = () => {
+
+    window.location.href = '/ingame.html';
+
+  }
+
 
 }).catch(console.error);
 
 /* -------------------------------------------------------- */
 
-document.getElementById('gotoLobby').addEventListener('click', function () {
-  window.location.href = '/lobby.html';
-});
+// document.getElementById('gotoLobby').addEventListener('click', function () {
+//   window.location.href = '/lobby.html';
+// });
 
-document.getElementById('gotoIngame').addEventListener('click', function () {
-  window.location.href = '/ingame.html';
-});
+// document.getElementById('gotoIngame').addEventListener('click', function () {
+//   window.location.href = '/ingame.html';
+// });
 
-document.getElementById('gotoIndex').addEventListener('click', function () {
-  window.location.href = '/index.html';
-});
+// document.getElementById('gotoIndex').addEventListener('click', function () {
+//   window.location.href = '/index.html';
+// });
 
-document.getElementById('gotosignup').addEventListener('click', function () {
-  window.location.href = '/signup.html';
-});
+// document.getElementById('gotosignup').addEventListener('click', function () {
+//   window.location.href = '/signup.html';
+// });
 
 /* -------------------------------------------------------- */
-
-export function renderCode(data) {
-  
-  console.log("hi");
-  console.log(data);
-
-  // Get each HTML element by its ID
-  const robotIdElement = document.getElementById('robotId');
-  const nameElement = document.getElementById('name');
-  const usernameElement = document.getElementById('username');
-  const codeElement = document.getElementById('code');
-
-  // Set each HTML element's text content
-  if (data.robotId != null) {
-    robotIdElement.textContent = 'Robot ID: ' + data.robotId;
-  } else {
-    robotIdElement.textContent = 'Robot ID: null';
-  }
-  nameElement.textContent = 'Name: ' + data.name;
-  usernameElement.textContent = 'Username: ' + data.username;
-  
-  if (data.code != null) {
-    codeElement.textContent = 'Code: ' + data.code;
-  } else {
-    codeElement.textContent = 'Code: null';
-  }
-
-  // Set each HTML element's color to white
-  robotIdElement.style.color = 'white';
-  nameElement.style.color = 'white';
-  usernameElement.style.color = 'white';
-  codeElement.style.color = 'white';
-
-}
--- src/main/resources/static/src/client/signup.js
+++ src/main/resources/static/src/client/signup.js
@@ -1,5 +1,5 @@
-import pixiApp from './pixi/app.js';
 import './css/bootstrap-reboot.css';
+import pixiApp from './pixi/app.js';
 import './css/signup.css';
 
 const wrapper = document.querySelector('.wrapper');
