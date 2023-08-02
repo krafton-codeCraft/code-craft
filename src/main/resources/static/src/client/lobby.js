724b5ed19d300e06dae41f0929300c9983fb5502
keyleeu <minhk.lee21@gmail.com>
2023-08-02 16:44:40 +0900

    325126236236

--- src/main/resources/static/src/client/lobby.js
+++ src/main/resources/static/src/client/lobby.js
@@ -2,8 +2,13 @@ import { downloadAssets } from './assets';
 import './css/bootstrap-reboot.css';
 import pixiApp from './pixi/app.js';
 import './css/lobby.css';
+import { getRobotInfos } from './networking';
 
 const startButton = document.getElementById('start-button');
+const robotIdElement = document.getElementById('robotId');
+const nameElement = document.getElementById('name');
+const usernameElement = document.getElementById('username');
+const codeElement = document.getElementById('code');
 
 let flag = true;
 
@@ -11,21 +16,12 @@ Promise.all([
 
   downloadAssets(),
   pixiApp(),
-  
+  getRobotInfos(),
 
 ]).then(() => {
 
   const username = sessionStorage.getItem('username');
   console.log(username);
-
-  startButton.onclick = () => {
-
-    window.location.href = '/ingame.html';
-
-  };
-
-
-
 
 }).catch(console.error);
 
@@ -46,3 +42,27 @@ document.getElementById('gotoIndex').addEventListener('click', function () {
 document.getElementById('gotosignup').addEventListener('click', function () {
   window.location.href = '/signup.html';
 });
+
+/* -------------------------------------------------------- */
+
+export function renderCode(data) {
+  // Check if the data is valid
+  // if (typeof data !== 'object' || data === null) {
+  //   console.error('Invalid data: ', data);
+  //   return;
+  // }
+
+  // // Set each HTML element's text content
+  // if (data.robotId != null) {
+  //   robotIdElement.textContent = data.robotId;
+  // } else {
+  //   robotIdElement.textContent = 'No robot ID';
+  // }
+  // nameElement.textContent = data.name;
+  // usernameElement.textContent = data.username;
+  // codeElement.textContent = data.code;
+
+  console.log("hi");
+  console.log(data);
+
+}
