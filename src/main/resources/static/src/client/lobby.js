79252945093b68b99264d73c9244dc393fc1e01a
keyleeu <minhk.lee21@gmail.com>
2023-08-03 22:59:13 +0900

    3521613462346

--- src/main/resources/static/src/client/lobby.js
+++ src/main/resources/static/src/client/lobby.js
@@ -1,8 +1,10 @@
 import { downloadAssets } from './assets';
 import './css/bootstrap-reboot.css';
 import pixiApp from './pixi/app.js';
-import './css/lobby.css';
-import './css/start-button.css'
+import './css/lobby/lobby.css';
+import './css/lobby/start-button.css';
+import './css/lobby/module-button.css';
+import './css/lobby/event-button.css';
 import { getRobotInfos } from './networking.js';
 
 // const startButton = document.getElementById('start-button');
