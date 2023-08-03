b80fea711a7808421a5a370c0d8224934aceb741
Jinu Jung <jjung7@berklee.edu>
2023-08-03 14:00:01 +0900

    adjust code interface size and loading

--- src/main/resources/static/src/client/htmlComponent/codeInterfaceLobby.js
+++ src/main/resources/static/src/client/htmlComponent/codeInterfaceLobby.js
@@ -1,52 +1,52 @@
-let editor;
-let selectedDeck = 0;
+let editorLobby;
+let selectedDeckLobby = 0;
 
 document.addEventListener('DOMContentLoaded', function () {
-
   require.config({ paths: { 'vs': 'https://unpkg.com/monaco-editor@latest/min/vs' } });
 
   let proxy = URL.createObjectURL(new Blob([`
-        self.MonacoEnvironment = {
-            baseUrl: 'https://unpkg.com/monaco-editor@latest/min/'
-        };
-        importScripts('https://unpkg.com/monaco-editor@latest/min/vs/base/worker/workerMain.js');
-    `], { type: 'text/javascript' }));
+      self.MonacoEnvironment = {
+          baseUrl: 'https://unpkg.com/monaco-editor@latest/min/'
+      };
+      importScripts('https://unpkg.com/monaco-editor@latest/min/vs/base/worker/workerMain.js');
+  `], { type: 'text/javascript' }));
 
   window.MonacoEnvironment = { getWorkerUrl: () => proxy };
 
   require(["vs/editor/editor.main"], function () {
-    editor = monaco.editor.create(document.getElementById('container-lobby-body'), {
+    editorLobby = monaco.editor.create(document.getElementById('container-lobby-body'), {
       value: '',
       language: 'java',
-      theme: 'vs-dark'
+      theme: 'vs-dark',
+      automaticLayout: true
     });
-  });
 
-  const url = `http://localhost:8080/get/robot-infos`;
-  fetch(url, {
-    method: 'GET'
-  })
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-      console.log(data[0].code);
-      editor.setValue(data[0].code);
+    // 에디터가 생성된 후 데이터를 불러옵니다.
+    const url = `http://localhost:8080/get/robot-infos`;
+    fetch(url, {
+      method: 'GET'
     })
-    .catch(e => {
-      console.log("ee?");
-    });
-
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        console.log(data[0].code);
+        editorLobby.setValue(data[0].code);
+      })
+      .catch(e => {
+        console.log("ee?");
+      });
+  });
 });
 
-function getEditorValue() {
-  if (editor) {
-      const content = editor.getValue();
+function getEditorValueLobby() {
+  if (editorLobby) {
+      const content = editorLobby.getValue();
       alert(content); // Display the content, you can do whatever you want with it
       return content;
   }
 }
 
-function code_check(result, status) {
+function code_check_lobby(result, status) {
   const terminal = document.getElementById('terminal-lobby');
   if (result != 0) {
       terminal.innerHTML = `<span style="font-weight: bold; color: red;" > ${status}: </span> <span style="font-weight: bold; color: red;">${result}</span>`;
@@ -57,6 +57,7 @@ function code_check(result, status) {
 }
 
 
-function selectDeckIndex(deckId) {
-  selectedDeck = deckId;
+function selectDeckIndexLobby(deckId) {
+  selectedDeckLobby = deckId;
+  console.log(selectedDeckLobby);
 }
