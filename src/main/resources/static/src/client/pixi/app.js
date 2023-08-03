a23026d73bdd2d41b8822558dbcbb933576e1191
creamcheezecat <92340465+creamcheezecat@users.noreply.github.com>
2023-08-04 01:42:17 +0900

    errortest2

--- src/main/resources/static/src/client/pixi/app.js
+++ src/main/resources/static/src/client/pixi/app.js
@@ -1,20 +1,19 @@
 import * as PIXI from 'pixi.js';
 import { renderBackground, makestar } from './background/background';
-import { playground,playgroundApp } from './playground/playground';
+import { playground, playgroundApp } from './playground/playground';
 import renderPlayer from './player/player';
 import renderBullet from './bullet/bullet';
 import { getCurrentState } from '../state';
 import { renderScan }  from './scan/scan';
 import { explosions } from './effect/explosion';
-import { particlesapp } from './effect/particles';
 
 export function pixiApp() {
   return new Promise((resolve) => {
-    const app = new PIXI.Application({ resizeTo: window});
+    const app = new PIXI.Application({ resizeTo: window });
     document.body.appendChild(app.view);
     makestar(app);
     playground();
-    document.body.appendChild(particlesapp);
+
     app.ticker.add((delta) => {
       playgroundApp.stage.removeChildren();
 
