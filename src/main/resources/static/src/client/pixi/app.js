7e2bc205e1b4ff111a7096179e558b6645b2ee98
creamcheezecat <92340465+creamcheezecat@users.noreply.github.com>
2023-08-04 22:00:27 +0900

    파티클 추가 및 폭발 삭제

--- src/main/resources/static/src/client/pixi/app.js
+++ src/main/resources/static/src/client/pixi/app.js
@@ -5,7 +5,7 @@ import renderPlayer from './player/player';
 import renderBullet from './bullet/bullet';
 import { getCurrentState } from '../state';
 import { renderScan }  from './scan/scan';
-import { explosions } from './effect/explosion';
+import { particletest } from './effect/particles';
 
 export function pixiApp() {
   return new Promise((resolve) => {
@@ -13,22 +13,13 @@ export function pixiApp() {
     document.body.appendChild(app.view);
     makestar(app);
     playground();
-
+    particletest(app);
     app.ticker.add((delta) => {
       playgroundApp.stage.removeChildren();
 
       const { robots,bullets,scans } = getCurrentState(); 
 
       renderBackground(app, delta);
-
-      if(explosions && explosions.length > 0){
-        explosions.forEach((explosion) => {
-          if(!explosion.playing){
-            explosion.gotoAndPlay(Math.random() * 26 | 0);
-          }
-          playgroundApp.stage.addChild(explosion);
-        })
-      }
       
       if(robots){
         robots.forEach((robot) => renderPlayer(robot,playgroundApp));
--- src/main/resources/static/src/client/pixi/effect/particles.js
+++ src/main/resources/static/src/client/pixi/effect/particles.js
@@ -1,66 +1,126 @@
-/* const stage = {
-    data : null, // imageData 를 저장해 두는데 사용됨
-    renderer : null, // 랜더러를 저장함.
-    mouseX : 0, // 갱신되는 mouseX 값을 저장하는데 사용됨
-    mouseY : 0, // 갱신되는 mouseY 값을 저장하는데 사용됨
-    frame: 0, // requestAnimationFrame 이 한번 실행될때마다 ++
+export function particletest(app){
+PIXI.settings.RESOLUTION = window.devicePixelRatio || 1;
 
-    sprite(){
-      return new PIXI.Sprite(texture);
-    },
+const sharpness = 0.1;
+const minDelta = 0.05;
 
-    createParticles (_x, _y){ // 인자로 최초에 파티클을 생성할 텍스트 기준의 x, y 좌표를 받는다.
-      const sprite = this.sprite(); // sprite 를 받는다.
-      const scale = 0.2 + Math.random() * .2; // 파티클의 배율을 지정한다.
-      const moveRadius = Math.random() * 10; // 파티클이 지속적으로 움직일 반지름의 값을 랜덤으로 지정한다.
-      const tint = Math.random() * 0xE68664; // 색상을 지정한다.
-      const x = canvasElement.width / 2 - this.textWidth / 2 + _x; // 캔바스를 기준으로 중심에 위치하도록 x 좌표를 구한다.
-      const y = canvasElement.height / 2 - this.textHeight / 2 + _y; // 캔바스를 기준으로 중심에 위치하도록 y 좌표를 구한다.
-      const speed = 5 + Math.random() * 5; // 파티클의 이동 속도를 랜덤함수를 이용해 산출 한다.
-      const set = { // 지정된 값들을 저장한다.
-        x,
-        y,
-        scale,
-        moveRadius,
-        tint,
-        speed,
-        _x,
-        _y
-      };
+const texture = createTexture(0, 8, app.renderer.resolution);
+const pointer = new PIXI.Point(app.screen.width / 2, app.screen.height / 2);
+const emitterPos = pointer.clone();
 
-      const add = {
-        moveRadius,
-      };
+const container = new PIXI.ParticleContainer(1000, {
+  scale: true,
+  position: true,
+  rotation: false,
+  uvs: false,
+  tint: true
+});
 
-      const move = ()  => { // 지속적으로 파티클이 움직이도록 하는 함수를 지정한다.
-        const check = clashCheck(set.x, set.y, this.mouseX, this.mouseY); // 마우스와의 충돌을 체크하고
-        if( check[0] ){ // 충돌했을 경우와
-          add.moveRadius = Math.min ( add.moveRadius + check[1]/5, 200 );
-        } else if( add.moveRadius > 0 ){ // 아닌경우를 지정.
-          add.moveRadius --;
-        } 
-        const x = set._x + Math.cos(this.frame / set.speed) * ( set.moveRadius + add.moveRadius);
-        const y = set._y + Math.sin(this.frame / set.speed) * ( set.moveRadius + add.moveRadius);
-        // 변경되는 좌표를 파티클에 각각 지정
-        sprite.x = canvasElement.width / 2 - this.textWidth / 2 + x;
-        sprite.y = canvasElement.height / 2 - this.textHeight / 2 + y;
+const emitter = new PIXI.particles.Emitter(container, [texture], {
+  autoUpdate: true,
+  alpha: {
+    start: 0.8,
+    end: 0.15
+  },
+  scale: {
+    start: 1,
+    end: 0.2,
+    minimumScaleMultiplier: 1
+  },
+  color: {
+    start: "#e3f9ff",
+    end: "#2196F3"
+  },
+  speed: {
+    start: 0,
+    end: 0,
+    minimumSpeedMultiplier: 1
+  },
+  acceleration: {
+    x: 0,
+    y: 0
+  },
+  maxSpeed: 0,
+  startRotation: {
+    min: 0,
+    max: 0
+  },
+  noRotation: true,
+  rotationSpeed: {
+    min: 0,
+    max: 0
+  },
+  lifetime: {
+    min: 0.6,
+    max: 0.6
+  },  
+  blendMode: "normal",
+  frequency: 0.0008,
+  emitterLifetime: -1,
+  maxParticles: 5000,
+  pos: {
+    x: 0,
+    y: 0
+  },
+  addAtBack: false,
+  spawnType: "point"
+});
+
+let resized = false;
+
+emitter.updateOwnerPos(emitterPos.x, emitterPos.y);
+
+app.stage.addChild(container);
+app.stage.interactive = true;
+app.ticker.add(onTick);
+app.stage.on("pointermove", event => pointer.copy(event.data.global));
+
+
+function onTick(delta) {
   
-        // 변경되는 좌표를 별도 set 값에 저장함. ( 마우스와 충돌시 점점 멀어지는 효과를 구현하기 위해 )
-        set.x = canvasElement.width / 2 - this.textWidth / 2 + set._x;
-        set.y = canvasElement.height / 2 - this.textHeight / 2 + set._y;
-      };
-      // 최초 생성시 x, ym scale, tint 값을 sprite 에 지정
-      sprite.x = x; 
-      sprite.y = y; 
-      sprite.scale.set(scale); 
-      sprite.tint = tint;
-  
-      // 해당 값들을 객체로 리턴함.
-      return {
-        sprite,
-        set,
-        add,
-        move
-      };
-    } // 여기까지 creatparticle
-  } */
+  if (!emitterPos.equals(pointer)) {
+        
+    const dt = 1 - Math.pow(1 - sharpness, delta); 
+    const dx = pointer.x - emitterPos.x;
+    const dy = pointer.y - emitterPos.y;
+    
+    if (Math.abs(dx) > minDelta) {
+      emitterPos.x += dx * dt;
+    } else {
+      emitterPos.x = pointer.x;
+    }
+
+    if (Math.abs(dy) > minDelta) {
+      emitterPos.y += dy * dt;
+    } else {
+      emitterPos.y = pointer.y;
+    }    
+    
+    emitter.updateOwnerPos(emitterPos.x, emitterPos.y);
+  }
+
+}
+
+  function createTexture(r1, r2, resolution) {
+      
+    const c = (r2 + 1) * resolution;
+    r1 *= resolution;
+    r2 *= resolution;
+      
+    const canvas = document.createElement("canvas");
+    const context = canvas.getContext("2d");
+    canvas.width = canvas.height = c * 2;
+    
+    const gradient = context.createRadialGradient(c, c, r1, c, c, r2);
+    gradient.addColorStop(0, "rgba(255,255,255,1)");
+    gradient.addColorStop(1, "rgba(255,255,255,0)");
+    
+    context.fillStyle = gradient;
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    
+    return PIXI.Texture.from(canvas);
+  }
+
+}
+
+
