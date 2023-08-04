6e75530a5c7b4ebe5da5b3bca1f4e0704ce98e7b
Jinu Jung <jjung7@berklee.edu>
2023-08-05 00:44:05 +0900

    speechbubble update

--- src/main/resources/static/src/client/chat.js
+++ src/main/resources/static/src/client/chat.js
@@ -1,3 +1,39 @@
-function handleChat(robotId, content){
-    //render chat
+export let robotMessages = {};
+
+
+export function handleChat(robotId, content){
+    const expirationTime = Date.now() + 2000;  // 2 seconds from now
+    robotMessages[robotId] = {
+        content: content,
+        expiresAt: expirationTime
+    };
+}
+
+export function renderSpeechBubble(robot, playgroundApp, content) {
+    const text = new PIXI.Text(content, {
+        fontFamily: 'Arial',
+        fontSize: 20,
+        fill: 'white',
+        align: 'center',
+        wordWrap: true,
+        wordWrapWidth: 150  // adjust this as needed
+    });
+
+    // Calculate the bubble size based on the text content
+    const bubbleWidth = text.width + 20; // adding some padding
+    const bubbleHeight = text.height + 10;
+
+    const bubble = new PIXI.Graphics();
+    drawRoundedRect(bubble, robot.x - bubbleWidth / 2, robot.y - 20 - bubbleHeight, bubbleWidth, bubbleHeight, 10);
+
+    text.x = robot.x - text.width / 2;
+    text.y = robot.y - 20 - bubbleHeight / 2 - text.height / 2;
+
+    playgroundApp.stage.addChild(bubble);
+    playgroundApp.stage.addChild(text);
+}
+function drawRoundedRect(graphics, x, y, width, height, radius) {
+    graphics.beginFill(0x000000, 0.7); // slightly transparent black
+    graphics.drawRoundedRect(x, y, width, height, radius);
+    graphics.endFill();
 }
--- src/main/resources/static/src/client/pixi/app.js
+++ src/main/resources/static/src/client/pixi/app.js
@@ -6,7 +6,8 @@ import renderBullet from './bullet/bullet';
 import { getCurrentState } from '../state';
 import { renderScan }  from './scan/scan';
 import { particletest } from './effect/particles';
-
+import { robotMessages } from '../chat';
+import { renderSpeechBubble } from '../chat';
 export function pixiApp() {
   return new Promise((resolve) => {
     const app = new PIXI.Application({ resizeTo: window });
@@ -21,9 +22,19 @@ export function pixiApp() {
 
       renderBackground(app, delta);
       
-      if(robots){
-        robots.forEach((robot) => renderPlayer(robot,playgroundApp));
-        scans.forEach((scan) => renderScan(scan,playgroundApp));
+      if (robots) {
+        robots.forEach((robot) => {
+          renderPlayer(robot, playgroundApp);
+
+          // Render speech bubble if there's a non-expired message for this robot
+          const robotMessage = robotMessages[robot.id];
+          if (robotMessage && robotMessage.expiresAt > Date.now()) {
+            renderSpeechBubble(robot, playgroundApp, robotMessage.content);
+          } else if (robotMessage && robotMessage.expiresAt <= Date.now()) {
+            delete robotMessages[robot.id];
+          }
+        });
+        scans.forEach((scan) => renderScan(scan, playgroundApp));
       }
 
       if (bullets) {
