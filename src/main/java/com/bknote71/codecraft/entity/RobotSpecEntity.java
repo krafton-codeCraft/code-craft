50092b033dca2b007c8c43cf240b3f73fadb216d
bknote71 <bknote71@gmail.com>
2023-08-05 15:23:03 +0900

    redirect from signup

--- src/main/java/com/bknote71/codecraft/entity/RobotSpecEntity.java
+++ src/main/java/com/bknote71/codecraft/entity/RobotSpecEntity.java
@@ -9,15 +9,25 @@ import javax.persistence.*;
 @Entity
 public class RobotSpecEntity {
 
-    @Id @GeneratedValue
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
     private String username;
     private String fullClassName;
+
+    @Column(columnDefinition = "LONGTEXT")
     private String code; // java code
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private UserEntity user;
+
+    // 외래 키 관계의 주인(UserEntity)이 변경될 때 같이 변경되도록 연관 관계 설정
+    public void setUser(UserEntity user) {
+        this.user = user;
+        if (!user.getSpecifications().contains(this)) { // 무한 루프 방지
+            user.getSpecifications().add(this);
+        }
+    }
 }
