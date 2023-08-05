e85e789e92a691e44992954fe85a888eb220ee60
keyleeu <minhk.lee21@gmail.com>
2023-08-05 19:20:24 +0900

    36126234142356

--- src/main/resources/static/src/client/lobby.js
+++ src/main/resources/static/src/client/lobby.js
@@ -6,6 +6,7 @@ import './css/lobby/start-button.css';
 import './css/lobby/module-button.css';
 import './css/compile-button.css';
 import './css/lobby/event-button.css';
+import './css/lobby/neon-button.css';
 import { getRobotInfos } from './networking.js';
 
 // const startButton = document.getElementById('start-button');
