3bac4265abcac061051de778432aadfbda3eff42
Jinu Jung <jjung7@berklee.edu>
2023-08-06 22:35:58 +0900

    compile code fix

--- src/main/resources/static/src/client/signup.js
+++ src/main/resources/static/src/client/signup.js
@@ -1,7 +1,6 @@
 import './css/bootstrap-reboot.css';
 import pixiApp from './pixi/app.js';
 import './css/signup.css';
-import { downloadAssets, getAsset } from './assets';
 
 const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
@@ -15,8 +14,7 @@ Promise.all([
     pixiApp(),
 
 ]).then(() => {
-    // const bgm = new Audio(getAsset('NLP_InGame_Final_Int.wav').src);
-    //     bgm.play();
+    
     registerLink.addEventListener('click', function () {
         wrapper.classList.add('active');
     });
