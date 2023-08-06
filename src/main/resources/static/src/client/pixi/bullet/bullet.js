f045414f4767988ae7e824d4c4a22f0f3a4e6ad9
creamcheezecat <92340465+creamcheezecat@users.noreply.github.com>
2023-08-06 18:00:32 +0900

    폭발 및 총알 파티클 추가

--- src/main/resources/static/src/client/pixi/bullet/bullet.js
+++ src/main/resources/static/src/client/pixi/bullet/bullet.js
@@ -1,22 +1,31 @@
 import { getAsset } from '../../assets';
-
+import { BoosterEffect } from '../effect/particles'
 const Constants = require('../../../shared/constants');
 const { BULLET_RADIUS } = Constants;
 
-
+export const effectbullets = {};
 export const bulletSprites = {};
 
 export function renderBullet(bullet, app) {
     const {id} = bullet;
 
     let bulletsprite = bulletSprites[id];
-
+    
     if(!bulletsprite){
+
         bulletsprite = createNewBulletSprite(bullet);
         bulletSprites[id] = bulletsprite;
         app.stage.addChild(bulletsprite);
+
+        const effectbullet = new BoosterEffect(app, bullet.x,bullet.y);
+        effectbullets[id] = effectbullet;
+
     }else{
+
         updateBulletSprite(bulletsprite,bullet);
+
+        effectbullets[id].moveTo(bullet.x,bullet.y);
+
     }
 }
 
