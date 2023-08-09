a26ae8c9ef1c3a8f8657a23ad683a1324561d605
bknote71 <bknote71@gmail.com>
2023-08-09 21:01:30 +0900

    bug fix after signup

--- src/main/java/com/bknote71/codecraft/entity/service/UserService.java
+++ src/main/java/com/bknote71/codecraft/entity/service/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -40,7 +41,7 @@ public class UserService {
 
     public boolean login(String username, String password) {
         UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(
-                username,
+                new User(username, password, List.of(new SimpleGrantedAuthority("ROLE_USER"))),
                 password,
                 List.of(new SimpleGrantedAuthority("ROLE_USER")));
 
