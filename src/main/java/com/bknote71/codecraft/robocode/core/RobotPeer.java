f5524670d22bc60c548bddbf02c9484fa43f7797
bknote71 <bknote71@gmail.com>
2023-08-11 13:14:00 +0900

    networking 8070

--- src/main/java/com/bknote71/codecraft/robocode/core/RobotPeer.java
+++ src/main/java/com/bknote71/codecraft/robocode/core/RobotPeer.java
@@ -325,6 +325,7 @@ public class RobotPeer {
 
 
     private void waitForUpdate() {
+        log.info("wait for update");
         synchronized (isSleeping) {
             // Notify the battle that we are now asleep.
             // This ends any pending wait() call in battle.runRound().
@@ -349,6 +350,7 @@ public class RobotPeer {
             // before the battle thread actually wakes up
             isSleeping.notifyAll();
         }
+        log.info("wait for update end");
     }
 
     // -----------
